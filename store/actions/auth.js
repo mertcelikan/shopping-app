1703931c67d73bdf574eda4fd9fce4c5bc84fda7
Mert Celikan <52200813+Mert98@users.noreply.github.com>
2020-05-19 02:27:14 +0300

    Store
    Actions and reducers

---
+++ store/actions/auth.js
@@ -0,0 +1,56 @@
+export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
+
+export const signup = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDQiPmSsi2K_EqcrsnISN1esKdjRnWoucA',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    // if (!response.ok) {
+    //   throw new Error('Something went wrong!');
+    // }
+
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+    return async dispatch => {
+      const response = await fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDQiPmSsi2K_EqcrsnISN1esKdjRnWoucA',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true
+          })
+        }
+      );
+  
+      // if (!response.ok) {
+      //   throw new Error('Something went wrong!');
+      // }
+  
+      const resData = await response.json();
+      console.log(resData);
+      dispatch({ type: LOGIN });
+    };
+  };
---
+++ store/actions/products.js
@@ -0,0 +1,128 @@
+import Product from '../../models/product';
+
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const CREATE_PRODUCT = 'CREATE_PRODUCT';
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+export const fetchProducts = () => {
+  return async dispatch => {
+    // any async code you want!
+    try {
+      const response = await fetch(
+        'https://kalemarket-app.firebaseio.com/products.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            'u1',
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price,
+            resData[key].category
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // send to custom analytics server
+      throw err;
+    }
+  };
+};
+
+export const deleteProduct = productId => {
+  return async dispatch => {
+    const response = await fetch(
+      `https://kalemarket-app.firebaseio.com/products/${productId}.json`,
+      {
+        method: 'DELETE'
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    dispatch({ type: DELETE_PRODUCT, pid: productId });
+  };
+};
+
+export const createProduct = (title, description, imageUrl, price,category) => {
+  return async dispatch => {
+    // any async code you want!
+    const response = await fetch(
+      'https://kalemarket-app.firebaseio.com/products.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price,
+          category
+        })
+      }
+    );
+
+    const resData = await response.json();
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price,
+        category: category
+      }
+    });
+  };
+};
+
+export const updateProduct = (id, title, description, imageUrl) => {
+  return async dispatch => {
+    const response = await fetch(
+      `https://kalemarket-app.firebaseio.com/products/${id}.json`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl
+      }
+    });
+  };
+};
